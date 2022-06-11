8ed0c9369de1185e9f34f3b8965a58d41a5e8b09
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-06-11 15:21:07 +0430

    feat: add faker js
    install faker js and init for genarate fake data

---
+++ db.js
@@ -0,0 +1,15 @@
+// import {faker} from '@faker-js/faker'
+const {faker} = require('@faker-js/faker');
+// const faker = require('faker');
+
+module.exports = () =>{
+    const data= {slider:[]};
+    for (let i = 0; i < 3; i++) {
+        data.slider.push({
+            id:faker.datatype.uuid(),
+            title:faker.name.firstName(),
+            slug:faker.lorem.slug(),
+        })    
+    }
+    return data;
+}
