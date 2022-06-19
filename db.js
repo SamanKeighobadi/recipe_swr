d603f89760133ef2306b7ed9ce30e4204e61fe48
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-06-19 19:34:18 +0430

    feat: users api
    implement users api for admin part

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
 const { faker } = require("@faker-js/faker");
 
 module.exports = () => {
-  const data = { slider: [], newest_recipes: [], popular_recipes: [] };
+  const data = {
+    slider: [],
+    newest_recipes: [],
+    popular_recipes: [],
+    users: [],
+    adimn: [],
+  };
+  // @desc slider item
+  // @route /slider
   for (let i = 0; i < 3; i++) {
     data.slider.push({
       id: faker.datatype.uuid(),
@@ -9,6 +17,8 @@ module.exports = () => {
       slug: faker.lorem.slug(),
     });
   }
+  // @desc latest recipes
+  // @route /newest_recipes
   for (let i = 0; i < 15; i++) {
     data.newest_recipes.push({
       id: faker.datatype.uuid(),
@@ -18,12 +28,27 @@ module.exports = () => {
     });
   }
 
+  // @desc popular recipes item
+  //  @route /popular_recipes
   for (let i = 0; i < 10; i++) {
     data.popular_recipes.push({
       id: faker.datatype.uuid(),
       title: faker.name.firstName(),
       text: faker.lorem.text(),
       date: faker.date.weekday(),
+    });
+  }
+  // @desc users list
+  // @route /users
+  for (let i = 0; i < 20; i++) {
+    data.users.push({
+      id: faker.datatype.number(),
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      address: {
+        country: faker.address.country(),
+        city: faker.address.city(),
+      },
     });
   }
 
